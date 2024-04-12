1eef910ee66483f841c0797111d33f975cbe1bfa
dac <dac@dypm.org>
2024-04-12 17:57:53 +0530

    Signed-off-by: Shubham <dac@dypm.org>

---
+++ code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a Nummber";
+    cin>>num;
+    int rem=0;
+    int og = num;
+    int last = 0;
+    while(rem=0){
+        last =  num/10;
+        
+ aehasc
+    }
+
+}
